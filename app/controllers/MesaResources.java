ef6bab46ed307cc03b928da4845fc03ae85f6cfa
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2011-09-21 13:35:51 -0600

    Added rest service to serve the eventos of one mesa
    Signed-off-by: Alexandro Blanco <ti3r.bubblenet@gmail.com>

--- app/controllers/MesaResources.java
+++ app/controllers/MesaResources.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import models.Mesa;
 import play.mvc.*;
+import models.Evento;
 
 public class MesaResources extends Controller {
 
@@ -19,6 +20,13 @@ public class MesaResources extends Controller {
     		renderJSON(m);
     	}else{
     		notFound("Mesas not found for the given id. "+id);
+    	}
+    }
+    
+    public static void getMesaEventos(Long id){
+    	List eventos = Evento.find("mesa.id = ? ",id).fetch();
+    	if (eventos != null & !eventos.isEmpty()){
+    		renderJSON(eventos);
     	}
     }
     
